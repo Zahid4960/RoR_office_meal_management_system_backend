364ad5a1f4d4d74d53434b3f30fec49d1470d904
zahid4960 <zahidcse4960@gmail.com>
2020-03-22 00:36:55 +0600

    save basic infos

--- app/controllers/api/v1/employee/basic_infos_controller.rb
+++ app/controllers/api/v1/employee/basic_infos_controller.rb
@@ -1,8 +1,30 @@
 class Api::V1::Employee::BasicInfosController < ApplicationController
 
-    def index
-        return 'hit'
-    end
+
     
+    def index
+        @basic_infos = ::Employee::BasicInfo.all
+        render json: {
+            status: 'success',
+            message: 'Data Found',
+            data: @basic_infos
+        }
+    end
+
+    def create   
+        @basic_info = ::Employee::BasicInfo.new(basic_info_params)   
+        if @basic_info.save    
+            render json: @basic_info
+        else   
+             render json: @basic_info.errors, status: :unprocessable_entity  
+        end 
+    end
+
+
+    private
+
+    def basic_info_params  
+        params.permit(:first_name, :middle_name, :last_name)  
+    end
     
 end
--- db/migrate/20200319064310_create_employee_basic_infos.rb
+++ db/migrate/20200319064310_create_employee_basic_infos.rb
@@ -5,16 +5,16 @@ class CreateEmployeeBasicInfos < ActiveRecord::Migration[5.2]
       t.string :first_name, null: false
       t.string :middle_name, null: true
       t.string :last_name, null: true
-      t.boolean :sex, null: false
-      t.string :dop, null: false
-      t.string :nid, null: false
-      t.string :father_name, null: false
-      t.string :mother_name, null: false
-      t.text :present_address, null: false
-      t.text :permanent_address, null: false
-      t.boolean :status, null: true
-      t.references :user, null: false
-      t.timestamps
+      # t.boolean :sex, null: false
+      # t.string :dop, null: false
+      # t.string :nid, null: false
+      # t.string :father_name, null: false
+      # t.string :mother_name, null: false
+      # t.text :present_address, null: false
+      # t.text :permanent_address, null: false
+      # t.boolean :status, null: true
+      # t.references :user, null: false
+      # t.timestamps
     end
   end
 end
