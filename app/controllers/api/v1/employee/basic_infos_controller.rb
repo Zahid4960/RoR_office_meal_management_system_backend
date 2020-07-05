0b8f5b842a89e58df7acdb4b588c41e75ecce76d
zahid4960 <zahidcse4960@gmail.com>
2020-07-05 23:05:43 +0600

    update

--- app/controllers/api/v1/employee/basic_infos_controller.rb
+++ app/controllers/api/v1/employee/basic_infos_controller.rb
@@ -6,9 +6,17 @@ class Api::V1::Employee::BasicInfosController < ApplicationController
         @basic_infos = ::Employee::BasicInfo.all
 
         if @basic_infos.blank?
-            render json: { status: 'success', message: 'No Data Found', data: @basic_infos }
+            render json: { 
+                status: 'success', 
+                message: 'No Data Found', 
+                data: @basic_infos 
+            }
         else
-         render json: { status: 'success', message: 'Data Found', data: @basic_infos }
+         render json: { 
+            status: 'success', 
+            message: 'Data Found', 
+            data: @basic_infos
+        }
         end
 
     end
@@ -16,7 +24,10 @@ class Api::V1::Employee::BasicInfosController < ApplicationController
     def create   
         @create_basic_info = ::Employee::BasicInfo.new(basic_info_params)   
         if @create_basic_info.save    
-            render json: { status: 'success', message: 'Data saved successfully' }
+            render json: { 
+                status: 'success', 
+                message: 'Data saved successfully' 
+            }
         # else   
         #      render json: { status: :unprocessable_entity, @basic_info.errors }
         end 
@@ -26,9 +37,16 @@ class Api::V1::Employee::BasicInfosController < ApplicationController
         begin
             @show_basic_info = ::Employee::BasicInfo.find(params[:id])
 
-            render json: { status: 'success', message: 'Data Found', data: @show_basic_info }
+            render json: { 
+                status: 'success', 
+                message: 'Data Found', 
+                data: @show_basic_info 
+            }
         rescue    
-            render json: { status: 'success', message: 'Data Not Found' }
+            render json: { 
+                status: 'success', 
+                message: 'Data Not Found' 
+            }
         end
     end
 
@@ -41,7 +59,20 @@ class Api::V1::Employee::BasicInfosController < ApplicationController
     private
 
     def basic_info_params  
-        params.permit(:first_name, :middle_name, :last_name, :sex, :dop, :nid, :father_name, :mother_name, :present_address, :permanent_address, :status, :user_id)  
+        params.permit(
+            :first_name, 
+            :middle_name, 
+            :last_name, 
+            :sex, 
+            :dop, 
+            :nid, 
+            :father_name, 
+            :mother_name, 
+            :present_address, 
+            :permanent_address, 
+            :status, 
+            :user_id
+        )  
     end
     
 end
