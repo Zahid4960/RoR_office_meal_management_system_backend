25a2f8c487314550e18227f49a02745db3491040
zahid4960 <zahidcse4960@gmail.com>
2020-07-05 09:30:31 +0600

    codebase modified

--- app/controllers/api/v1/employee/basic_infos_controller.rb
+++ app/controllers/api/v1/employee/basic_infos_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::Employee::BasicInfosController < ApplicationController
 
 
-    
     def index
         @basic_infos = ::Employee::BasicInfo.all
-
         if @basic_infos.blank?
             render json: { status: 'success', message: 'No Data Found', data: @basic_infos }
         else
@@ -13,35 +11,61 @@ class Api::V1::Employee::BasicInfosController < ApplicationController
 
     end
 
-    def create   
-        @create_basic_info = ::Employee::BasicInfo.new(basic_info_params)   
-        if @create_basic_info.save    
+
+    def create
+        @create_basic_info = ::Employee::BasicInfo.new(basic_info_params)
+        if @create_basic_info.save
             render json: { status: 'success', message: 'Data saved successfully' }
-        # else   
-        #      render json: { status: :unprocessable_entity, @basic_info.errors }
-        end 
+        end
+
     end
+
 
     def show
         begin
             @show_basic_info = ::Employee::BasicInfo.find(params[:id])
-
             render json: { status: 'success', message: 'Data Found', data: @show_basic_info }
-        rescue    
+        rescue
             render json: { status: 'success', message: 'Data Not Found' }
         end
     end
 
-    def update
-    end
+
+    # def update
+    #   begin
+    #     @find_basic_info = ::Employee::BasicInfo.find(params[:id])
+    #       if !@find_basic_info.blank?
+    #         @update_basic_info = ::Employee::BasicInfo.update(@find_basic_info)
+    #           if @update_basic_info
+    #             render json: { status: 'success', message: 'Data Updated successfully', data: @update_basic_info }
+    #           else
+    #             render json: { status: 'Failed', message: 'Data Failed To Update' }
+    #           end
+    #       end
+    #   rescue
+    #     render json: { status: 'success', message: 'Data Not Found' }
+    #   end
+    # end
+
 
     def destroy
+      begin
+        @find_basic_info = ::Employee::BasicInfo.find(params[:id])
+        if @find_basic_info.destroy
+          render json: { status: 'success', 'message': 'Data Deleted successfully' }
+        else
+          render json: { status: 'Failed', 'message': 'Data Failed To Delete' }
+        end
+      rescue
+        render json: { status: 'Exception', 'message': 'Invalid Route' }
+      end
     end
+
+
 
     private
-
-    def basic_info_params  
-        params.permit(:first_name, :middle_name, :last_name, :sex, :dop, :nid, :father_name, :mother_name, :present_address, :permanent_address, :status, :user_id)  
+    def basic_info_params
+        params.permit(:first_name, :middle_name, :last_name, :sex, :dop, :nid, :father_name, :mother_name, :present_address, :permanent_address, :status, :user_id)
     end
-    
+
 end
