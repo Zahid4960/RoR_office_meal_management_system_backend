bc4096e8ff2bb8f0374b621f1f083f8f1d9214dd
zahid4960 <zahidcse4960@gmail.com>
2020-08-12 14:50:01 +0600

    codebase updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-
-  # resources :basic_info, controller: 'api/v1/employee/basic-info'
-  namespace :api do
-    namespace :v1 do
-      namespace :employee do
-        resources :basic_infos
-      end
-    end
-  end
-
+  #Authentication
+  get "registration" => "users#registration"
+  # namespace :api do
+  #   namespace :v1 do
+  #     namespace :employee do
+  #       resources :basic_infos
+  #     end
+  #   end
+  # end
 end
