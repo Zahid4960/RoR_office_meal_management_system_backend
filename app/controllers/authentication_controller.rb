688aaf223098d5e412b1d165fdcff5757a432c5f
zahid4960 <zahidcse4960@gmail.com>
2020-08-12 18:48:57 +0600

    codebase updated

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,10 +1,23 @@
 class AuthenticationController < ApplicationController
   # registration
   def registration
-    logger.debug 'Balsal'
-    # render json: {
-    #     status: 'success',
-    #     message: 'No Data Found'
-    # }
+    render json: 'ok'
+    render json: {
+        status: 'success',
+        message: 'No Data Found'
+    }
+  end
+
+  private
+  def registration_params
+    params.require(
+      :email,
+      :user_name,
+      :password
+    ).permit(
+      :email,
+      :user_name,
+      :password
+    )
   end
 end
