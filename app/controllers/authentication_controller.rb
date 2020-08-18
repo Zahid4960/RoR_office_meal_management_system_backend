e04aa73748027e64f0210c5f052294beb47eb85e
zahid4960 <zahidcse4960@gmail.com>
2020-08-18 18:59:39 +0600

    codebase updated

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,23 +1,37 @@
 class AuthenticationController < ApplicationController
-  # registration
-  def registration
-    render json: 'ok'
-    render json: {
+
+# registration method
+def registration
+  begin
+    @registration_data = User.new(registration_params)
+    # save
+    if @registration_data.save
+      render json: {
         status: 'success',
-        message: 'No Data Found'
+        message: 'User Registration Successful!!',
+        data: @registration_data
+      }
+    end
+  rescue
+    render json: {
+      status: 'failed',
+      message: 'Something Went Wrong!!!',
+      data: @registration_data.errors
     }
   end
+end
 
-  private
-  def registration_params
-    params.require(
-      :email,
-      :user_name,
-      :password
-    ).permit(
-      :email,
-      :user_name,
-      :password
-    )
-  end
+
+# private methods
+private
+
+# method for registration_params
+def registration_params
+  params.permit(
+    :email,
+    :user_name,
+    :password
+  )
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # regular expression for email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # email -> lowercase before save
+  before_save { self.email = email.downcase }
+
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :user_name, presence: true,  uniqueness: true
+  validates :password, presence: true, length: { minimum: 6}
 end
