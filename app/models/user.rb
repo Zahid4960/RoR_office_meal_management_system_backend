fe85456846a636eaa65ae9989b668af639783784
zahid4960 <zahidcse4960@gmail.com>
2020-08-30 18:47:39 +0600

    codebase updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   # regular expression for email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-
-
   # validations
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :user_name, presence: true,  uniqueness: true, length: { minimum: 6 }
