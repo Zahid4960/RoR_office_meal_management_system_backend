6524d803282d360aca23c4847cbb34cf43fb255a
zahid4960 <zahidcse4960@gmail.com>
2020-09-27 12:17:51 +0600

    authentication system updated

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,39 +1,40 @@
 class AuthenticationController < ApplicationController
 
 def registration
-  registration_data = User.new(registration_params)
-  registration_data[:plain_password] = registration_data[:password]
-  encrypted_password = BCrypt::Password.create(registration_data[:password])
-  registration_data[:password] = encrypted_password
+  @registration_data = User.new(registration_params)
 
-  if registration_data.save
+  @registration_data[:plain_password] = @registration_data[:password]
+  @encrypted_password = BCrypt::Password.create(@registration_data[:password])
+  @registration_data[:password] = @encrypted_password
+
+  if @registration_data.save
     render json: {
       status: 'success',
       message: 'Registration Successful!!',
-      data: registration_data
+      data: @registration_data
     }
   else
     render json: {
       status: 'error',
       message: 'Registration Not Successful!!',
-      errors: registration_data.errors
+      errors: @registration_data.errors
     }
   end
 end
 
 
 def login
-  user_data = User.where(email: login_params[:email]).first
-  if user_data.blank?
+  @user_data = User.where(email: login_params[:email]).first
+  if @user_data.blank?
     render json: {
       staus: 'error',
       message: 'Invalid Email!!'
     }
 
   else
-    if BCrypt::Password.new(user_data.password) == login_params[:password]
-      session[:current_user_id] = user_data.id
-      session[:current_user_email] = user_data.email
+    if BCrypt::Password.new(@user_data.password) == login_params[:password]
+      session[:current_user_id] = @user_data.id
+      session[:current_user_email] = @user_data.email
       render json: {
         status: 'success',
         message: 'Successfully loged In!!'
