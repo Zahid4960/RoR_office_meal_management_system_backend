5b501456dc3309a3c422585417da0de0830a2aa3
zahid4960 <zahidcse4960@gmail.com>
2020-11-08 15:09:55 +0600

    index method for designation

--- app/controllers/designation_controller.rb
+++ app/controllers/designation_controller.rb
@@ -1,9 +1,29 @@
 class DesignationController < ApplicationController
 
   def index
+    @limit = params['limit'] != nil ? params['limit'] : 10
+    @designation_lists = Designation.paginate(page: params[:page], :per_page => @limit)
+    render json: {
+        message: 'success',
+        data: @designation_lists
+    }
   end
 
   def create
+    @designation_data = Designation.new(designation_params)
+
+    if @designation_data.save
+      render json: {
+          status: 'success',
+          message: 'Designation Data Saved Successfully!!!',
+          data: @designation_data
+      }
+    else
+      render json: {
+          status: 'error',
+          message: @designation_data.errors
+      }
+    end
   end
 
   def show
@@ -13,6 +33,14 @@ class DesignationController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def designation_params
+    params.permit(
+        :designation_name
+    )
   end
 
 end
